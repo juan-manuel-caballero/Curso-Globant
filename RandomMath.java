7b57c32ca880439f2f61d5e2cae99c6e004185d8
Juan Manuel Caballero <caballerojm.88@gmail.com>
2024-10-22 20:27:18 -0300

    Modificaciones con try

--- RandomMath.java
+++ RandomMath.java
@@ -7,19 +7,20 @@ public class RandomMath {
         int min;
         int max;
         int opc;
-        Scanner myScan = new Scanner(System.in);
+        try (Scanner myScan = new Scanner(System.in)) {
+            System.out.println("Coloque el máximo del rango de números");
+            max = myScan.nextInt();
+            System.out.println("Coloque el mínimo del rango de números");
+            min = myScan.nextInt();
 
-        System.out.println("Coloque el máximo del rango de números");
-        max = myScan.nextInt();
-        System.out.println("Coloque el mínimo del rango de números");
-        min = myScan.nextInt();
+            num1 = Math.random() * (max - min + 1) + min;
 
-        num1 = Math.random() * (max - min + 1) + min;
+            System.out.println("Su número es: " + num1);
 
-        System.out.println("Su número es: " + num1);
+            System.out.println("Redondear para arriba(1) - para abajo(2)");
+            opc = myScan.nextInt();
+        }
 
-        System.out.println("Redondear para arriba(1) - para abajo(2)");
-        opc = myScan.nextInt();
         if (opc == 1) {
             System.out.println(Math.ceil(num1));
         } else {
--- ValidacionContrasenna.java
+++ ValidacionContrasenna.java
@@ -3,17 +3,16 @@ import java.util.Scanner;
 public class ValidacionContrasenna {
 
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
-
-        System.out.println("Validación de contraseña:");
-        String contrasenna = myScanner.nextLine();
-        String correcta = "secreto";
-
-        while (!contrasenna.equals(correcta)) {
-            System.out.println("La contraseña es incorrecta intente nuevamente: ");
-            contrasenna = myScanner.nextLine();
+        try (Scanner myScanner = new Scanner(System.in)) {
+            System.out.println("Validación de contraseña:");
+            String contrasenna = myScanner.nextLine();
+            String correcta = "secreto";
+            
+            while (!contrasenna.equals(correcta)) {
+                System.out.println("La contraseña es incorrecta intente nuevamente: ");
+                contrasenna = myScanner.nextLine();
+            }
+            System.out.println("La contraseña es correcta Bienvenido!");
         }
-        System.out.println("La contraseña es correcta Bienvenido!");
-        myScanner.close();
     }
 }
