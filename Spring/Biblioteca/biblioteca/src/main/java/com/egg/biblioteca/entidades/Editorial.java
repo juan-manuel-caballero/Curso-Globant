bf6bdb1f281255e1a54f802f42bdcfe03e0f724e
Juan Manuel Caballero <caballerojm.88@gmail.com>
2025-02-25 20:39:28 -0300

    Repositorio de libro y creación del resto

---
+++ Spring/Biblioteca/biblioteca/src/main/java/com/egg/biblioteca/entidades/Editorial.java
@@ -0,0 +1,35 @@
+package com.egg.biblioteca.entidades;
+
+import java.util.UUID;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Editorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(name = "nombre")
+    private String nombre;
+
+    public Editorial() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
